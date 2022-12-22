176bb881a86918e6c30ca297d4363ad214c479c4
kerokerogeorge <kerokerogeorge@yahoo.co.jp>
2022-12-22 18:48:47 +0900

    moved to handler

--- handler/wait_cancel.go
+++ handler/wait_cancel.go
@@ -7,6 +7,7 @@ import (
 )
 
 func WaitCancel() {
+	// # キャンセル通知があるまで処理を待機する場合、context.Context.Doneメソッドで得られるチャネルからの通知を待つ
 	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
 	defer cancel()
 	go func() { fmt.Println("another goroutine") }()
